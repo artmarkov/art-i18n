e99f7e035f5f07c34ed68b987a978a9dfd5a2d48
artmarkov <artmarkov@mail.ru>
2019-03-24 13:39:37 +0300

    Update m160419_235120_i18n_ru_art_settings.php

--- ru/m160419_235120_i18n_ru_art_settings.php
+++ ru/m160419_235120_i18n_ru_art_settings.php
@@ -27,6 +27,13 @@ class m160419_235120_i18n_ru_art_settings extends TranslatedMessagesMigration
             'Date Format' => 'Формат Даты',
             'Time Format' => 'Формат Времени',
             'Page Size' => 'Записей на странице',
+            'Admin Phone' => 'Телефон Администратора',
+            'Admin Facebook' => 'Facebook Администратора',
+            'Admin Instagram' => 'Instagram Администратора',
+            'Phone Mask' => 'Маска Телефона',
+            'Date Mask' => 'Маска Даты',
+            'Time Mask' => 'Маска Времени',
+            'Date & Time Mask' => 'Маска Даты и Времени',
         ];
     }
 }
